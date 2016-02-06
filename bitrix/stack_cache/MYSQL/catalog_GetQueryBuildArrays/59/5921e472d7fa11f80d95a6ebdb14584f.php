5d7ee4e5acfb224c13001c8a34609414733e209f
Kalashnikov Konstantin <k-kalashnikov@inbox.ru>
2016-02-06 18:41:58 +0300

    first

---
+++ bitrix/stack_cache/MYSQL/catalog_GetQueryBuildArrays/59/5921e472d7fa11f80d95a6ebdb14584f.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001454772181';
+$dateexpire = '001454782981';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:17:"P_1,2,3,4_,1,1_ru";a:1:{i:0;a:4:{s:2:"ID";s:1:"1";s:18:"CATALOG_GROUP_NAME";s:14:"Розничная цена";s:18:"CATALOG_CAN_ACCESS";s:1:"Y";s:15:"CATALOG_CAN_BUY";s:1:"Y";}}s:15:"P_1,2,3,4_,1_ru";a:1:{i:0;a:4:{s:2:"ID";s:1:"1";s:18:"CATALOG_GROUP_NAME";s:14:"Розничная цена";s:18:"CATALOG_CAN_ACCESS";s:1:"Y";s:15:"CATALOG_CAN_BUY";s:1:"Y";}}}}';
+return true;
+?>
